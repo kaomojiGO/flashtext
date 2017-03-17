edf7bbf931e81a9917de88bec1132e6213e3932e
kaomojiGO <kaomoji_freemail@yahoo.co.jp>
2017-03-18 04:29:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 var msg;
 var msgs;
 var runcheck=0;
-var count;
+var count=0;
 var flashspeed;
-var d1;
-var d2;
    
 function flash(){
    flashspeed=document.getElementById("speed").value;
-   if(runcheck==0){
-      runcheck=1;
-      msg=document.getElementById("text").value;
-      msg=msg.replace(/。/g , " ");
-      msg=msg.replace(/、/g , " ");
-      msgs=msg.split(" ");
-      for(count=0;count<msgs.length;count++){
-         if(count > msgs.length-1){
-            count=0;
-            runcheck=0;
-            return;
-         }
-         document.getElementById("msgbox").innerHTML=msgs[count];
-         d1 = new Date();
-         while (true) {
-            d2 = new Date();
-            if (d2 - d1 > flashspeed) {
-               break;
-            }
-         }
+   msg=document.getElementById("text").value;
+   msg=msg.replace(/。/g , " ");
+   msg=msg.replace(/、/g , " ");
+   msgs=msg.split(" ");
+   for(count=0;count<msgs.length;count++){
+      if(count > msgs.length-1){
+         count=0;
+         runcheck=0;
+         return;
       }
+      document.getElementById("msgbox").innerHTML=msgs[count++];
+      setTimeout("flash()",flashspeed);
    }
 }
