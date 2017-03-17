32d63ce6763d964cc326eac3fd42508cbd9a28fd
kaomojiGO <kaomoji_freemail@yahoo.co.jp>
2017-03-18 04:07:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var msg;
 var msgs;
+var runcheck=0;
 var count=0;
 var flashspeed;
    
 function flash(){
-   msg=document.getElementById("text").value;
-   msg=msg.replace(/。/g , " ");
-   msg=msg.replace(/、/g , " ");
-   msgs=msg.split(" ");
    flashspeed=document.getElementById("speed").value;
-   if(count > msgs.length-1){
-      count=0;
-      return;
+   if(runcheck==0){
+      runcheck=1;
+      msg=document.getElementById("text").value;
+      msg=msg.replace(/。/g , " ");
+      msg=msg.replace(/、/g , " ");
+      msgs=msg.split(" ");
+      for(count<msgs.length){
+         if(count > msgs.length-1){
+            count=0;
+            runcheck=0;
+            return;
+         }
+         document.getElementById("msgbox").innerHTML=msgs[count++];
+         const d1 = new Date();
+         while (true) {
+            const d2 = new Date();
+            if (d2 - d1 > flashspeed) {
+               break;
+            }
+         }
+      }
    }
-   document.getElementById("msgbox").innerHTML=msgs[count++];
-   setTimeout("flash()",flashspeed);
 }
