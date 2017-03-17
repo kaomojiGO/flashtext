962c98a654d80678075d6d067b1085e00bec7194
kaomojiGO <kaomoji_freemail@yahoo.co.jp>
2017-03-18 01:58:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-<script type="text/JavaScript">
-   msg="一定間隔で,文字が,変更されていますか？";
-   msgs=msg.split(",");
-   i=0;
+var msg="一定間隔で,文字が,変更されていますか？";
+var msgs=msg.split(",");
+var i=0;
    
-   function changeMsg(){
+function changeMsg(){
    if(i > msgs.length-1){
-   i=0;
+      i=0;
    }
    document.getElementById("msgbox").innerHTML=msgs[i++];
    setTimeout("changeMsg()",1000);
-   }
-</script>
+}
