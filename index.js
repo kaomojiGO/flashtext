f95215b1ecc52e1c8bd4f05e8b702658b5a2d36b
kaomojiGO <kaomoji_freemail@yahoo.co.jp>
2017-03-18 07:20:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 var msg;
 var msgs;
-var runcheck=0;
 var count=0;
 var flashspeed;
+var runcheck=0;
+var halftext
    
 function flash(){
-   flashspeed=document.getElementById("speed").value;
-   msg=document.getElementById("text").value;
-   msg=msg.replace(/。/g , " ");
-   msg=msg.replace(/、/g , " ");
-   msgs=msg.split(" ");
-   for(count=0;count<msgs.length;count++){
-      if(count > msgs.length-1){
-         count=0;
-         runcheck=0;
-         return;
+   document.getElementById("msgbox").innerHTML=msgs[count++];
+}
+
+function flashloop(){
+   if(runcheck==0){
+      runcheck=1;
+      flashspeed=document.getElementById("speed").value;
+      msg=document.getElementById("text").value;
+      msg=msg.replace(/。/g , "。 ");
+      msg=msg.replace(/、/g , "、 ");
+      msgs=msg.split(" ");
+      for(var i=0;i<msgs.length-1;i++){
+         if(msgs[i].length>20){
+            halftext=msgs[i].substr(msgs[i].length-msgs[i].length/2);
+            msgs[i]=msgs[i].substr(0,msgs[i].length-msgs[i].length/2);
+            msgs.splice(i+1, 0, halftext);
+         }
       }
-      document.getElementById("msgbox").innerHTML=msgs[count++];
-      setTimeout("flash()",flashspeed);
+            
+      for(var i=0;i<msgs.length-1;i++){
+         setTimeout("flash()",flashspeed*i);
+      }
+      count=0;
+      setTimeout("release()",flashspeed*(msgs.length-1)+500);
    }
 }
+
+function release(){
+   document.getElementById("msgbox").innerHTML="おしまい";
+   runcheck=0;
+}
+
